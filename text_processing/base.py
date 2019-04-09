f2cdd1bdabcf849189f18848dbe57ed85666e0f6
Anton Krylov <anthony.kryloff@gmail.com>
2019-04-09 12:54:37 +0300

    improve preprocessing, add experimental and slow features

--- text_processing/base.py
+++ text_processing/base.py
@@ -1,5 +1,6 @@
 import re
 
+from natasha import MoneyExtractor, OrganisationExtractor, DatesExtractor
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.pipeline import Pipeline
 
@@ -11,11 +12,16 @@ codex_regexes = {
     re.compile(r'кодекс[а-я]*\s+(об\s+)?административн[а-я]*\s+правонарушени[а-я]*', re.IGNORECASE | re.MULTILINE): 'КоАП',
 }
 
-CAP_SPACES = re.compile(r'((?:[А-Я] +){2,}[А-Я])', re.IGNORECASE)
+money = MoneyExtractor()
+dates = DatesExtractor()
+org = OrganisationExtractor()
+
+
+CAP_SPACES = re.compile(r'\s((?:[А-Я]\s+){2,}[А-Я][^\w])', re.IGNORECASE)
 
 
 def fix_cap_spaces(text: str):
-    return CAP_SPACES.sub(lambda m: m.group(1).replace(' ', ''), text)
+    return CAP_SPACES.sub(lambda m: ' '+m.group(1).replace(' ', '')+' ', text)
 
 
 def remove_newlines(text: str):
@@ -24,18 +30,31 @@ def remove_newlines(text: str):
 
 
 def remove_numbers(text: str):
-    text = re.sub(r'\d[\d\s]+руб\.( +\d\d коп\.)?', 'SUM', text)
-    text = re.sub(r'\d[\d\s]*(?:,\d\d) рубл[а-я]+', 'SUM', text)
+    # for match in reversed(money(text)):
+    #     text = text[:match.span[0]] + 'SUM' + text[match.span[1]:]
+    #
+    # for match in reversed(dates(text)):
+    #     text = text[:match.span[0]] + 'DATE' + text[match.span[1]:]
+    text = re.sub(r'\d[\d\s]+([,.]\d\d\s*)?руб(\.|л[а-я]+)(\s+\d\d\s*коп(\.|[а-я]+))?', 'SUM', text)
     text = re.sub(r'\d{5,}', 'NUM', text)
     # text = re.sub(r'\d+', 'NUM', text)
     return text
 
 
+def enum_orgs(text: str):
+    names = set()
+    for match in reversed(org(text)):
+        name = match.fact.name
+        names.add(name)
+        idx = list(names).index(match.fact.name)
+        text = text[:match.span[0]] + 'ORG%d' % (idx,) + text[match.span[1]:]
+
+    return text
+
+
 def cut_parts(text: str) -> str:
-    if not re.findall(r'установил:\s*\n', text, re.IGNORECASE | re.MULTILINE):
-        print("NO MAIN PART")
-    no_head = re.sub(r'^.*\n\s*установил:\s*\n', '', text, 1, re.IGNORECASE | re.DOTALL)
-    no_resolution = re.sub(r'\n\s*решил:\s*\n.*$', '', no_head, 1, re.IGNORECASE | re.DOTALL)
+    no_head = re.sub(r'^.*установил\s*:\s*\n', '', text, 1, re.IGNORECASE | re.DOTALL)
+    no_resolution = re.sub(r'(\n|суд)\s*решил\s*:\s*\n.*$', '', no_head, 1, re.IGNORECASE | re.DOTALL)
     return no_resolution
 
 
@@ -49,6 +68,7 @@ def preprocess(text: str) -> str:
 
     text = remove_newlines(text)
     text = remove_numbers(text)
+    # text = enum_orgs(text)
     return text
 
 
--- text_processing/simple.py
+++ text_processing/simple.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 import bs4
@@ -8,11 +9,22 @@ from text_processing.base import preprocess
 def parse(path):
     with open(path, 'r') as f:
         markup = '\n'.join(f)
-        soup = bs4.BeautifulSoup(markup)
+        soup = bs4.BeautifulSoup(markup, "lxml")
         return preprocess(soup.text)
 
 
+def parse_all(basedir):
+    for f in Path(basedir).rglob("*.html"):
+        with open(f, 'r') as fd:
+            markup = '\n'.join(fd)
+            soup = bs4.BeautifulSoup(markup, "lxml")
+            if not re.findall(r'установил\s*:\s*\n', soup.text, re.IGNORECASE | re.MULTILINE):
+                continue
+            if re.search(r'Судебный акт принят в закрытом судебном заседании', soup.text):
+                continue
+            yield preprocess(soup.text)
+
+
 if __name__ == '__main__':
-    for f in Path("../out/docs_simple2").rglob("*.html"):
-        text = parse(f)
-        print(text)
+    for doc in parse_all("../out/docs_simple2"):
+        print(doc)
