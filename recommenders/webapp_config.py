245f8932d5231ee8282ae3c6df1dacb5b3b7a10c
Anton Krylov <anthony.kryloff@gmail.com>
2019-05-21 23:16:41 +0300

    fix webapp config

--- recommenders/webapp_config.py
+++ recommenders/webapp_config.py
@@ -4,19 +4,17 @@ root_dir = os.path.dirname(os.path.abspath(__file__)) + "/.."
 
 UCI_FOLDER = os.environ.get('UCI_FOLDER', root_dir + '/out')
 DOCS_LOCATION = os.environ.get('DOCS_LOCATION', UCI_FOLDER + '/corpus.uci')
-N_SAMPLES = int(os.environ.get('N_SAMPLES', 20000))
 N_TOPICS = int(os.environ.get('N_TOPICS', 800))
 
 LSI_PICKLE = os.environ.get('LSI_PICKLE', UCI_FOLDER + '/lsi.pickle')
 LDA_PICKLE = os.environ.get('LDA_PICKLE', UCI_FOLDER + '/lda.pickle')
 D2V_PICKLE = os.environ.get('D2V_PICKLE', UCI_FOLDER + '/d2v.pickle')
-ARTM_PICKLE = os.environ.get('ARTM_PICKLE', UCI_FOLDER + '/artm.pickle')
-ARTM2_PICKLE = os.environ.get('ARTM2_PICKLE', UCI_FOLDER + '/artm_2.pickle')
+ARTM_PICKLE = os.environ.get('ARTM_PICKLE', UCI_FOLDER + '/artm_new.pkl')
 
 RESTFUL_JSON = {
     'ensure_ascii': False,
     'indent': 4
 }
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
